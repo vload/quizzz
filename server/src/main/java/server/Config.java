b98d645942e0df68706b7bc938621b5e3d2715b9
Vlad Ştefănescu <v.t.stefanescu@student.tudelft.nl>
2022-02-25 16:16:41 +0100

    Fix checkstyle issues.

--- server/src/main/java/server/Config.java
+++ server/src/main/java/server/Config.java
@@ -17,8 +17,6 @@ package server;
 
 import java.util.Random;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
