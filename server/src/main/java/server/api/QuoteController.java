b5c80a2851824206ec85e51a4fce975d4a8a13ca
Henry Page <h.page@student.tudelft.nl>
2022-02-28 00:37:42 +0100

    Fixing the API endpoint for the TEMPLATE project

--- server/src/main/java/server/api/QuoteController.java
+++ server/src/main/java/server/api/QuoteController.java
@@ -65,7 +65,7 @@ public class QuoteController {
         if (id < 0 || !repo.existsById(id)) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(repo.getById(id));
+        return ResponseEntity.ok(repo.findById(id).get());
     }
 
     /**
@@ -101,7 +101,9 @@ public class QuoteController {
      */
     @GetMapping("rnd")
     public ResponseEntity<Quote> getRandom() {
+        var quotes = repo.findAll();
         var idx = random.nextInt((int) repo.count());
-        return ResponseEntity.ok(repo.getById((long) idx));
+        return ResponseEntity.ok(quotes.get(idx));
+//        return ResponseEntity.ok(repo.getById((long) idx));
     }
 }
