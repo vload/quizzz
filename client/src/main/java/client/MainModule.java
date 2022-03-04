05a482ad1d0c2b8052c36efd821f631c7bca5f97
Andrei Dascalu <A.Dascalu@student.tudelft.nl>
2022-03-04 15:22:42 +0100

    Fixed pipeline

--- client/src/main/java/client/MainModule.java
+++ client/src/main/java/client/MainModule.java
@@ -1,6 +1,7 @@
 package client;
 
 import client.scenes.MainScreenCtrl;
+import client.scenes.NameScreenCtrl;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
@@ -11,5 +12,6 @@ public class MainModule implements Module {
     @Override
     public void configure(Binder binder) {
         binder.bind(MainScreenCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(NameScreenCtrl.class).in(Scopes.SINGLETON);
     }
 }
