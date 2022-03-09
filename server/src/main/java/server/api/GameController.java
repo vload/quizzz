946d3806241346f66e7db5d2aa62ac610d2619ec
Henry Page <h.page@student.tudelft.nl>
2022-03-09 18:18:48 +0100

    Fixing checkstyle violations

--- server/src/main/java/server/api/GameController.java
+++ server/src/main/java/server/api/GameController.java
@@ -1,6 +1,5 @@
 package server.api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 import java.util.function.Consumer;
 
-import org.springframework.web.context.request.async.DeferredResult;
 import server.server_classes.*;
 
 
