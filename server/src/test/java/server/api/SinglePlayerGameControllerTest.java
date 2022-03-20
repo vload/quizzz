b202ca545d97bb6b761d2e588c3be26e5b40dc63
Vlad Ştefănescu <v.t.stefanescu@student.tudelft.nl>
2022-03-20 17:06:27 +0100

    Fix checkstyle

--- server/src/test/java/server/api/SinglePlayerGameControllerTest.java
+++ server/src/test/java/server/api/SinglePlayerGameControllerTest.java
@@ -7,7 +7,6 @@ import commons.Submission;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
