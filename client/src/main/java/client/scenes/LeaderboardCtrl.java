ea81a31e7a25d4b69a881785109d04f3d851986f
Tom Huisman <t.m.huisman@student.tudelft.nl>
2022-03-24 23:21:37 +0100

    Auto stash before merge of "main" and "origin/main"

--- client/src/main/java/client/scenes/LeaderboardCtrl.java
+++ client/src/main/java/client/scenes/LeaderboardCtrl.java
@@ -11,22 +11,9 @@ import javax.inject.Inject;
 
 public class LeaderboardCtrl extends AbstractCtrl{
 
-    public boolean endGameLeaderBoard;
-
     @FXML
     private ListView<String> leaderBoardList;
 
-    @FXML
-    private Label topText;
-
-    @FXML
-    private Label bottomText;
-
-    @FXML
-    private Button lobbyButton;
-
-    @FXML
-    private Button quitButton;
 
     private final MyMainCtrl myMainCtrl;
 
@@ -48,32 +35,12 @@ public class LeaderboardCtrl extends AbstractCtrl{
     }
 
 
-    /**
-     * Shows the lobby screen when you press the lobby button
-     */
-    public void showLobbyScreen(){
-    }
-
 
     /**
      * Show the right things on the screen if it is the endgame leaderboard or the midgame leaderboard
      * Fills the leaderboard
      */
     public void init(){
-        endGameLeaderBoard = true;
-
-        //if you want to show the endgame leaderboard
-        if(endGameLeaderBoard) {
-            topText.setText("End Game");
-            bottomText.setOpacity(0.0d);
-        } else { //if you want to show the midgame leaderboard
-            topText.setText("Mid Game");
-            bottomText.setText("Game Continues in 5 seconds");
-            lobbyButton.setDisable(true);
-            quitButton.setDisable(true);
-            lobbyButton.setOpacity(0.0d);
-            quitButton.setOpacity(0.0d);
-        }
 
         ObservableList<String> players = FXCollections.observableArrayList(
                 "335 points - Julia", "423 points - Ian", "123 points - Sue", "859 points - Matthew",
