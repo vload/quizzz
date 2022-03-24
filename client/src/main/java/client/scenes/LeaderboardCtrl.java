894278661975bea7614b1fafc7d31ba4ec9b5f99
Tom Huisman <t.m.huisman@student.tudelft.nl>
2022-03-24 23:26:34 +0100

    checkstyle

--- client/src/main/java/client/scenes/LeaderboardCtrl.java
+++ client/src/main/java/client/scenes/LeaderboardCtrl.java
@@ -4,8 +4,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
 import javax.inject.Inject;
 
 
