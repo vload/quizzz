d52119581eda4ef5c64505034f72030254024791
Henry Page <h.page@student.tudelft.nl>
2022-03-24 16:35:25 +0100

    The Wrapper for the Time Reduction

--- commons/src/main/java/commons/poll_wrapper/TimeReductionPollObject.java
+++ commons/src/main/java/commons/poll_wrapper/TimeReductionPollObject.java
@@ -1,8 +1,15 @@
 package commons.poll_wrapper;
 
+import commons.PlayerData;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
 public class TimeReductionPollObject implements MultiPlayerPollObject {
 
-    private Long reductionWeight;
+    private PlayerData whoInitiated;
 
 
     /**
@@ -15,14 +22,45 @@ public class TimeReductionPollObject implements MultiPlayerPollObject {
     /**
      * Constructor for TimeReductionPollObject
      *
-     * @param reductionWeight The factor that the timers of other clients will be reduced by
+     * @param whoInitiated The PlayerData object of the person who initiated this.
      */
-    public TimeReductionPollObject(Long reductionWeight) {
-        this.reductionWeight = reductionWeight;
+    public TimeReductionPollObject(PlayerData whoInitiated) {
+        this.whoInitiated = whoInitiated;
     }
 
     @Override
     public Object getBody() {
-        return null;
+        return whoInitiated;
+    }
+
+    /**
+     * Enhanced equals method
+     *
+     * @param o The object to be compared to
+     * @return true if the two Objects have tested equals.
+     */
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    /**
+     * Enhanced hashcode method
+     *
+     * @return the hashcode
+     */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * Enhanced toString
+     *
+     * @return A string representation of this object
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
     }
 }
