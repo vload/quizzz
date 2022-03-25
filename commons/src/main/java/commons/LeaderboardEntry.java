f5c1d2a93ab8b5278673bceb81691d2c978a5fc7
Tom Huisman <t.m.huisman@student.tudelft.nl>
2022-03-25 17:24:25 +0100

    Checkstyle again

--- commons/src/main/java/commons/LeaderboardEntry.java
+++ commons/src/main/java/commons/LeaderboardEntry.java
@@ -33,12 +33,10 @@ public class LeaderboardEntry {
     /**
      * Constructor for LeaderboardEntry
      *
-     * @param id ID of the entry
      * @param name The name of the player
      * @param score The score of the player
      */
     public LeaderboardEntry(String name,long score) {
-        //this.id = id;
         this.name = name;
         this.score = score;
     }
