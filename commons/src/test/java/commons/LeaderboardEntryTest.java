5751d711ae4d11237178904e954a40453125f626
Tom Huisman <t.m.huisman@student.tudelft.nl>
2022-03-25 17:28:24 +0100

    some tests failed

--- commons/src/test/java/commons/LeaderboardEntryTest.java
+++ commons/src/test/java/commons/LeaderboardEntryTest.java
@@ -12,7 +12,7 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void checkConstructor() {
-        var le = new LeaderboardEntry(1,"name", 100);
+        var le = new LeaderboardEntry("name", 100);
         assertEquals("name", le.getName());
         assertEquals(100, le.getScore());
     }
@@ -22,7 +22,7 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void getNameTest(){
-        var le = new LeaderboardEntry(1,"name", 100);
+        var le = new LeaderboardEntry("name", 100);
         assertEquals("name", le.getName());
     }
 
@@ -31,7 +31,7 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void getScoreTest(){
-        var le = new LeaderboardEntry(1,"name", 100);
+        var le = new LeaderboardEntry("name", 100);
         assertEquals(100, le.getScore());
     }
 
@@ -40,8 +40,8 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void equalsHashCode() {
-        var le1 = new LeaderboardEntry(1,"name", 100);
-        var le2 = new LeaderboardEntry(1,"name", 100);
+        var le1 = new LeaderboardEntry("name", 100);
+        var le2 = new LeaderboardEntry("name", 100);
         assertEquals(le1, le2);
         assertEquals(le1.hashCode(), le2.hashCode());
     }
@@ -51,8 +51,8 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void notEqualsHashCode() {
-        var le1 = new LeaderboardEntry(1, "name1", 101);
-        var le2 = new LeaderboardEntry(2,"name2", 102);
+        var le1 = new LeaderboardEntry( "name1", 101);
+        var le2 = new LeaderboardEntry("name2", 102);
         assertNotEquals(le1, le2);
         assertNotEquals(le1.hashCode(), le2.hashCode());
     }
@@ -62,7 +62,7 @@ public class LeaderboardEntryTest {
      */
     @Test
     public void hasToString() {
-        var le1 = new LeaderboardEntry(1,"name", 100).toString();
+        var le1 = new LeaderboardEntry("name", 100).toString();
         assertTrue(le1.contains(LeaderboardEntry.class.getSimpleName()));
         assertTrue(le1.contains("\n"));
         assertTrue(le1.contains("name"));
