6e5e8e08cd02d35bc271095d04b7eb46a14e0670
Matyas Kollert <M.Kollert@student.tudelft.nl>
2022-03-26 16:44:20 +0100

    Fix checkstyle

--- commons/src/main/java/commons/PlayerData.java
+++ commons/src/main/java/commons/PlayerData.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import java.io.Serializable;
 import java.util.*;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
