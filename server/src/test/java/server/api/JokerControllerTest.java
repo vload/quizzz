a840c2fe43d623b4f4d574adc51f1d13cc9e7969
Vlad Ştefănescu <v.t.stefanescu@student.tudelft.nl>
2022-03-31 15:40:27 +0200

    Add test file for JokerController

---
+++ server/src/test/java/server/api/JokerControllerTest.java
@@ -0,0 +1,16 @@
+package server.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JokerControllerTest {
+
+    @Test
+    void useJokerMultiplayer() {
+    }
+
+    @Test
+    void useJokerSingleplayer() {
+    }
+}
